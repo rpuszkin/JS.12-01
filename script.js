94d3492c0e0a7fcc8ddb13085e9e8b9d2768071c
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-09-18 18:52:20 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+class Apple {
+  constructor(name, color, sweetness) {
+    this.name = name;
+    this.color = color;
+    this.sweetness = sweetness;
+  }
+}
+
+const apple = new Apple("antonowka", "yellow", 5);
+
+//przykładowe użycie:
+console.log(apple.name); //antonówka
+console.log(apple.color); //yellow
+console.log(apple.sweetness); //5
